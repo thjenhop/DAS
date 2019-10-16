46d8c2903863410d6b184f3ac49eca0008ffee02
hopdt1404 <thjenhop99@gmail.com>
2019-10-16 22:59:51 +0700

    my code dfs_proble in geeksforgeeks

---
+++ Graph/3_dfs_graph/dfs_problem.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        
+        int N, E;
+        cin>>N>>E;
+        vector<int> adj[N];
+        bool vis[N] = {false};
+        for(int i=0;i<E;i++)
+        {
+            int u,v;
+            cin>>u>>v;
+            adj[u].push_back(v);
+        }
+        
+        bfs(0, adj, vis, N);
+        cout<<endl;
+    }
+}
+
+void bfs(int s, vector<int> adj[], bool vis[], int N)
+{
+   
+    queue<int> queue;
+    queue.push(s);
+    vis[s] = true;
+    while(queue.empty() == false){
+        int currentVertex = queue.front();
+        queue.pop();
+        cout << currentVertex << " ";
+        vector<int>::iterator i;
+        for(i = adj[currentVertex].begin(); i != adj[currentVertex].end(); i++){
+            if(vis[*i] == false){
+                vis[*i] = true;
+                queue.push(*i);
+            }
+        }
+        
+    }
+}
